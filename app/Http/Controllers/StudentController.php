b67c242b74481b10dd3532cfdbf8e86207d7572f
Elen <elen@buycycle.com>
2025-02-05 00:07:03 +0400

    dynamic dropdown data for sections select after class select change

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ClassesResource;
+use App\Http\Resources\SectionResource;
 use App\Http\Resources\StudentResource;
+use App\Models\Classes;
+use App\Models\Section;
 use App\Models\Student;
-use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
@@ -13,6 +16,17 @@ class StudentController extends Controller
         $students = StudentResource::collection(Student::paginate(10));
         return inertia('Students/Index', [
             'students' => $students
+        ]);
+    }
+
+    public function create()
+    {
+        $classes = ClassesResource::collection(Classes::all());
+        $sections = SectionResource::collection(Section::all());
+
+        return inertia('Students/Create', [
+            'classes' => $classes,
+            'sections' => $sections,
         ]);
     }
 }
--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -2,6 +2,7 @@
 
 import MagnifyingGlass from "@/Components/Icons/MagnifyingGlass.vue";
 import Pagination from "@/Components/Pagination.vue";
+import {Link} from "@inertiajs/vue3";
 
 defineProps({
     students : {
@@ -34,8 +35,7 @@ defineProps({
                         </div>
 
                         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                            <Link
-                                :href="route('students.create')"
+                            <Link :href="route('students.create')"
                                 class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                             >
                                 Add Student
